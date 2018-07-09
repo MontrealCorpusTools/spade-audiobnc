80c2536a6a48eb96eb251ed3ace69fb8f28878f8
Michael GOODALE <mgooda2@roquefort.cs.mcgill.ca>
2018-07-09 12:58:10 -0400

    Fixed do_pipeline

--- do_pipeline.sh
+++ do_pipeline.sh
@@ -4,13 +4,14 @@ CORPUS_DIR=corpus_for_mfa
 PRONUNCIATION_DICTIONARY=pronunciation.txt
 MFA_DIR=/home/linguistics/mgooda2/montreal-forced-aligner/bin
 
-#rm corpus_for_mfa/*
-#rm pronunciation.txt
-#python3 output_dictionary.py $(ls input/ | sed 's/^/textgrid\//')
-#python3 output_mfa_formatted.py $(ls input/ | sed 's/^/textgrid\//')
-#cd $MFA_DIR
-#./mfa_align $PREFIX$CORPUS_DIR $PREFIX$PRONUNCIATION_DICTIONARY english $PREFIX/output -n -j 20
-#cd $PREFIX
+mkdir -p corpus_for_mfa output cleaned_textgrids classify_grids speakered_textgrids_chunked out_with_labels
+rm corpus_for_mfa/*
+rm pronunciation.txt
+python3 output_dictionary.py $(ls input/ | sed 's/^/textgrid\//')
+python3 output_mfa_formatted.py $(ls input/ | sed 's/^/textgrid\//')
+cd $MFA_DIR
+./mfa_align $PREFIX$CORPUS_DIR $PREFIX$PRONUNCIATION_DICTIONARY english $PREFIX/output -n -j 20
+cd $PREFIX
 python3 aligner-difference.py
 python3 classify.py $(find classify_grids -type f)
 python3 speaker_data.py $(find out_with_labels -type f)
